2a9e4b07d208f95c652c04858e0575b5c2cbec17
aditya.kumar <aditya1944@live.com>
2022-12-30 19:17:50 +0530

    added solution for day 10 part 2

---
+++ aoc2022/10/10.2/go/main.go
@@ -0,0 +1,119 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+
+	readFile, err := os.Open("../../input");
+	if err != nil {
+		log.Fatalf("file could not open, err : %v", err);
+	}
+	fileScanner := bufio.NewScanner(readFile);
+
+	fileScanner.Split(bufio.ScanLines);
+
+	var (
+		matx = buildMatx()
+		registerVal = 1
+		cycle = 0
+		crtRowIndex = 0
+		crtColIndex = 0
+	)
+
+
+
+	for fileScanner.Scan() {
+		line := fileScanner.Text();
+		tokens := strings.Split(line, " ");
+
+		
+
+		switch tokens[0] {
+		case "addx":
+			draw(crtRowIndex, crtColIndex, registerVal, &matx)
+
+			cycle +=1
+
+			crtColIndex += 1
+			if crtColIndex == 40 {
+				crtRowIndex += 1
+				crtColIndex = 0;
+			}
+
+
+			draw(crtRowIndex, crtColIndex, registerVal, &matx)
+
+
+			cycle +=1
+
+
+			crtColIndex += 1
+			if crtColIndex == 40 {
+				crtRowIndex += 1
+				crtColIndex = 0;
+			}
+
+
+			inputVal, err := strconv.Atoi(tokens[1])
+			if err != nil {
+				log.Fatalf("error in reading val, err : %v", err)
+			}
+
+			registerVal += inputVal;
+
+			
+			draw(crtRowIndex, crtColIndex, registerVal, &matx)
+		case "noop":
+			
+			draw(crtRowIndex, crtColIndex, registerVal, &matx)
+
+			cycle+=1
+			crtColIndex += 1
+			if crtColIndex == 40 {
+				crtRowIndex += 1
+				crtColIndex = 0;
+			}
+
+			draw(crtRowIndex, crtColIndex, registerVal, &matx)
+		}
+	}
+
+	for _, row := range matx {
+		for _, val := range row {
+			fmt.Printf("%q ",val)
+		}
+		fmt.Println()
+	}
+
+}
+
+func draw(crtR, crtC, sprite int, matx *[6][40]byte) {
+
+	if crtC == sprite-1 {
+		matx[crtR][crtC] = '#'
+	} else if crtC == sprite {
+		matx[crtR][crtC] = '#'
+	} else if crtC == sprite+1{
+		matx[crtR][crtC] = '#'
+	}
+}
+
+
+func buildMatx() [6][40]byte {
+	matx := [6][40]byte{};
+
+	for rowIndex, row := range matx{
+		for colIndex := range row {
+			matx[rowIndex][colIndex] = '.';
+		} 
+	}
+
+	return matx;
+}
